47fa193984f9b3bc78d3b0b7fb66d45d13ba3cdd
classob11 <classob1108@outlook.com>
2022-02-19 16:13:12 -0500

    updated

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.btnm').addEventListener('click',() =>{
+    document.querySelector('.bl-menu').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.sect');
+ScrollReveal().reveal('.cards', {delay:500});
+ScrollReveal().reveal('.banner', {delay:500});
+ScrollReveal().reveal('.banner2', {delay:500});
+ScrollReveal().reveal('.social', {delay:500});
